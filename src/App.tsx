a2db48b6e8d1f0a0dfd6bb5123f9e30b2488bf26
Marvin Bangemann <88746983+bngmnn@users.noreply.github.com>
2025-04-06 17:11:26 +0200

    App.tsx aktualisieren

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<InviteePage />} /> {/* Your existing component */}
                 <Route path="/invitations" element={<InvitationList />} /> {/* New route to see all invitations */}
+                <Route path="/dresscode" element={<Dresscode />} />
             </Routes>
         </Router>
     );
