f9df91dcc6d75ea705d13834a1fb12a3cd0d49c8
Marvin Bangemann <88746983+bngmnn@users.noreply.github.com>
2025-04-06 10:15:54 +0200

    InviteePage.tsx aktualisieren

--- src/InviteePage.tsx
+++ src/InviteePage.tsx
@@ -126,8 +126,8 @@ function InviteePage() {
       <main className='flex flex-col items-center w-screen min-h-screen font-serif text-gray-700/90 overflow-x-hidden overflow-y-auto px-8 pb-8'>
         <LogoArea />
         <div className='w-full overflow-hidden max-w-full fixed top-0 left-0 h-full'>
-          <div className="bg-radial from-amber-100 via-transparent to-transparent w-[600px] h-full translate-y-1/2 scale-125 absolute bottom-0 -z-10 overflow-x-hidden"></div>  
-          <div className="bg-radial from-stone-300 via-transparent to-transparent w-[800px] h-full translate-y-1/3 left-0 scale-125 absolute bottom-0 -z-10 overflow-x-hidden"></div>  
+          <div className="bg-radial from-amber-200 via-transparent to-transparent w-[600px] h-full translate-y-1/2 scale-125 absolute bottom-0 -z-10 overflow-x-hidden"></div>  
+          <div className="bg-radial from-red-200 via-transparent to-transparent w-[800px] h-full translate-y-1/3 left-0 scale-125 absolute bottom-0 -z-10 overflow-x-hidden"></div>  
         </div>
         <div className="relative z-0 bg-transparent mt-8">
           {isLoading && <p className="text-center">Loading...</p>}
