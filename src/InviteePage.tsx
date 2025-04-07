013d3d23448c6035695b15c0f7f82e232a4b4409
Marvin Bangemann <88746983+bngmnn@users.noreply.github.com>
2025-04-07 15:49:29 +0000

    another one

--- src/InviteePage.tsx
+++ src/InviteePage.tsx
@@ -124,7 +124,7 @@ function InviteePage() {
   return (
     <>
       <main className='flex flex-col items-center w-screen min-h-screen font-serif text-gray-700/90 overflow-x-hidden overflow-y-auto px-8 pb-8'>
-        <LogoArea />
+        <LogoArea animateLogo={!userId} />
         <div className='w-full overflow-hidden max-w-full fixed top-0 left-0 h-full'>
           <div className="bg-radial from-amber-200 via-transparent to-transparent w-[600px] h-full translate-y-1/2 scale-125 absolute bottom-0 -z-10 overflow-x-hidden"></div>  
           <div className="bg-radial from-red-200 via-transparent to-transparent w-[800px] h-full translate-y-1/3 left-0 scale-125 absolute bottom-0 -z-10 overflow-x-hidden"></div>  
--- src/LogoArea.tsx
+++ src/LogoArea.tsx
@@ -1,8 +1,11 @@
-export const LogoArea = () => {
+type LogoArea = {
+  animateLogo: boolean;
+}
+export const LogoArea = ({animateLogo}: LogoArea) => {
   return (
     <>
-    <div className="bg-[url('/stag.png')] bg-contain bg-center w-full h-screen bg-no-repeat flex items-center justify-center text-center mix-blend-multiply relative z-10 animate-[logoarea_4s_ease-in-out_forwards]">
-        <div className="backdrop-blur-sm py-4 px-4 text-lg md:text-2xl text-center flex items-center justify-center flex-col absolute z-20 bottom-0">
+    <div className={`bg-[url('/stag.png')] bg-contain bg-center w-full h-screen bg-no-repeat flex items-center justify-center text-center mix-blend-multiply relative z-10 ${animateLogo ? 'animate-[logoarea_4s_ease-in-out_forwards]' : ''}`}>
+        <div className="backdrop-blur-sm py-4 px-4 text-lg md:text-2xl text-center flex items-center justify-center flex-col absolute z-20 bottom-0 w-full">
             <strong className="text-balance block uppercase w-full first-letter:text-xl first-letter:md:text-3xl">Marvins Birthday</strong>
             <strong className="font-cursive text-white tracking-widest drop-shadow-[-2px_-2px_1px_rgba(0,0,0,.25),2px_2px_1px_rgba(0,0,0,.75)] my-2 text-lg sm:text-xl md:text-3xl">White Stag Festival</strong>
             <p className="text-sm">14. Juni 2025</p>
